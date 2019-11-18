6c1b3458fc1cf012c6d69103c3d74a446b5017da
Regan <regannitz@gmail.com>
2019-11-18 10:05:16 -0500

    adding git ignore

--- RedBadge.Models/Team/TeamEdit.cs
+++ RedBadge.Models/Team/TeamEdit.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,9 @@ namespace RedBadge.Models
         public int TeamID { get; set; }
         [Required]
         public Guid UserID { get; set; }
+        [ForeignKey("CalendarVariable")]
+        public int CalendarID { get; set; }
+        public virtual Calendar CalendarVariable { get; set; }
         [Required]
         [Display(Name = "Team Name")]
         public string TeamName { get; set; }
--- RedBadge.Services/ProfileService.cs
+++ RedBadge.Services/ProfileService.cs
@@ -1,4 +1,5 @@
-﻿using RedBadgeProject.Data;
+﻿using RedBadge.Models;
+using RedBadgeProject.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
